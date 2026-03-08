01576e91aee1a7d366c2d73f7ed27bcd0f6164cc
oljas <medetbaevoljas@gmail.com>
2026-03-08 19:58:32 +0100

    home work 11 complete

--- home-works/hw-11/main.js
+++ home-works/hw-11/main.js
@@ -1,4 +1,25 @@
-const loginName = document.getElementById("loginName");
-const passField = document.getElementById("passField");
+const login = "test@test.com";
+const pass = "11111111";
 
-console.log(loginName);
+const signInForm = document.getElementById("signInForm");
+
+signInForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let loginName = document.getElementById("loginName").value;
+  let passField = document.getElementById("passField").value;
+
+  if (loginName === login && passField === pass) {
+    alert("Sign in is successfull");
+    signInForm.reset();
+  } else {
+    alert("Your login or password doesn't match");
+    signInForm.reset();
+  }
+});
+
+// Задание 1
+
+// Используя JavaScript и DOM, напишите скрипт, который будет проверять, совпадают ли введенные значения с заранее заданными значениями логина и пароля (их нужно самому придумать и заранее указать в скрипте).
+
+// Если значения совпадают, выведите сообщение об успешной аутентификации, в противном случае - сообщение об ошибке.
