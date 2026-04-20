7bcf2c099eef1144df526f5e4601964af967d0ae
Oljas <medetbaevoljas@gmail.com>
2026-04-20 17:22:51 +0200

    home work 19.1

---
+++ home-works/hw-19.1/main.js
@@ -0,0 +1,33 @@
+const multyColorBox = document.getElementById("multyColorBox");
+
+const colors = ["#ff0000", "#00ff00", "#0000ff"];
+const text = ["I ", "am ", "disco", "🕺"];
+let emptyArray = "";
+let count = 0;
+
+function randomNumber(countCeil) {
+  return Math.floor(Math.random() * countCeil);
+}
+
+function changeColor(int) {
+  setInterval(function () {
+    multyColorBox.style.backgroundColor = colors[randomNumber(colors.length)];
+  }, int);
+}
+
+function showText() {
+  if (count >= text.length) {
+    console.log("Text is shown");
+    return;
+  }
+
+  emptyArray = emptyArray + text[count];
+  count++;
+  multyColorBox.textContent = emptyArray;
+
+  setTimeout(showText, 1000);
+}
+
+showText();
+changeColor(1000);
+// showText(1000, text.length);
